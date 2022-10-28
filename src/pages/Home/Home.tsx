f0cff9b5280fcc7b801cfb73213da97f24aae483
Xiang <wiwi19991214@gmail.com>
2022-10-28 17:46:41 +0800

    adjust navigate link

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -5,11 +5,11 @@ function Home() {
     <>
       <h1 className='text-center text-[48px] font-medium'>請選擇想去的頁面</h1>
       <div className='flex gap-[40px] pt-5 justify-center'>
-        <NavigateButton text={"點我去Sasa page"} navigateTarget={"reserve"} />
         <NavigateButton
-          text={"點我去搶購頁面"}
+          text={"點我去Sasa page"}
           navigateTarget={"productDetail"}
         />
+        <NavigateButton text={"點我去搶購頁面"} navigateTarget={"reserve"} />
       </div>
     </>
   );
