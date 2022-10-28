dfea26e4403d9a495d6763b97d0240a2e6619ac8
Xiang <wiwi19991214@gmail.com>
2022-10-28 15:40:53 +0800

    setup project

---
+++ tailwind.config.js
@@ -0,0 +1,24 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
+  theme: {
+    screens: {
+      sm: "375px",
+      xl: "1440px",
+    },
+    extend: {
+      colors: {
+        primaryRed: "#ff5353",
+        primaryBlue: "#428bca",
+        primaryPageBackgroundGray: "#f2f2f2",
+        secondaryPageBackgroundGray: "#fafafa",
+        primaryGray: "#545454",
+        secondaryGary: "#666666",
+        thirdGray: "#999999",
+        hintGray: "#616161",
+        primaryBrown: "#c89185",
+      },
+    },
+  },
+  plugins: [],
+};
