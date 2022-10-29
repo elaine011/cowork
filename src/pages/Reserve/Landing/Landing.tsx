0c678a5209eaa316453785d387349b34f4767dba
Xiang <wiwi19991214@gmail.com>
2022-10-29 19:26:34 +0800

    ActivityStatement component complete

--- src/pages/Reserve/Landing/Landing.tsx
+++ src/pages/Reserve/Landing/Landing.tsx
@@ -1,11 +1,15 @@
 import Header from "./Header";
 import ReserveProcess from "./ReserveProcess";
+import productsAll from "../../../images/reserve-products/all.png";
+import ActivityStatement from "../../../components/Common/ActivityStatement";
 
 function Landing() {
   return (
     <div className='bg-secondaryPageBackgroundGray px-[10px]'>
       <Header />
       <ReserveProcess />
+      <img src={productsAll} alt='carouselImg' />
+      <ActivityStatement />
     </div>
   );
 }
