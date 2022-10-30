9cbb2ac39f1988df6d146b08281d3184119eb28b
Xiang <wiwi19991214@gmail.com>
2022-10-30 14:38:10 +0800

    replace footer with ReserveFooter in Landing page

--- src/pages/Reserve/Landing/Landing.tsx
+++ src/pages/Reserve/Landing/Landing.tsx
@@ -2,11 +2,9 @@ import Header from "./Header";
 import ReserveProcess from "./ReserveProcess";
 import productsAll from "../../../images/reserve-products/all.png";
 import ActivityStatement from "../../../components/Common/ActivityStatement";
-import FunctionButton from "../../../components/Common/FunctionButton";
-import { useNavigate } from "react-router-dom";
+import ReserveFooter from "../../../components/Footers/ReserveFooter";
 
 function Landing() {
-  const navigate = useNavigate();
   return (
     <>
       <div className='fixed right-0 left-0 top-0 bottom-0 bg-secondaryPageBackgroundGray z-[-50]'></div>
@@ -24,18 +22,11 @@ function Landing() {
         <div className='mb-[100px]'>
           <ActivityStatement />
         </div>
-        <div className='fixed bg-white bottom-0 left-0 right-0 p-[15px]'>
-          <FunctionButton
-            text={"搶先登記"}
-            textColor={"white"}
-            textPosition={"center"}
-            bgColor={"#ff5353"}
-            letterSpacing={2}
-            width={"full"}
-            clickFn={() => navigate("/reserve/submitForm")}
-          />
-        </div>
       </main>
+      <ReserveFooter
+        switchRoute={"/reserve/submitForm"}
+        functionButtonText={"搶先登記"}
+      />
     </>
   );
 }
