6f36ff25b8107feeaf3d40962bb1e345ad706f41
Xiang <wiwi19991214@gmail.com>
2022-10-31 09:16:04 +0800

    adjust page backgroundColor

--- src/pages/Reserve/Landing/Landing.tsx
+++ src/pages/Reserve/Landing/Landing.tsx
@@ -8,32 +8,35 @@ import { useNavigate } from "react-router-dom";
 function Landing() {
   const navigate = useNavigate();
   return (
-    <div className='md:pt-[144px] max-w-[1100px] mx-auto bg-secondaryPageBackgroundGray px-[10px]'>
-      <div className='md:flex md:mb-[126px] items-center justify-between'>
-        <div className='md:order-2 md:w-[572px]'>
-          <Header />
-          <ReserveProcess />
+    <>
+      <div className='fixed right-0 left-0 top-0 bottom-0 bg-secondaryPageBackgroundGray z-[-50]'></div>
+      <div className='md:pt-[144px] max-w-[1100px] mx-auto bg-secondaryPageBackgroundGray px-[10px]'>
+        <div className='md:flex md:mb-[126px] items-center justify-between'>
+          <div className='md:order-2 md:w-[572px]'>
+            <Header />
+            <ReserveProcess />
+          </div>
+          <div className='md:order-1 md:ml-[2vw]'>
+            <img src={productsAll} alt='carouselImg' className='w-[374px]' />
+          </div>
         </div>
-        <div className='md:order-1 md:ml-[2vw]'>
-          <img src={productsAll} alt='carouselImg' className='w-[374px]' />
-        </div>
-      </div>
 
-      <div className='mb-[100px]'>
-        <ActivityStatement />
+        <div className='mb-[100px]'>
+          <ActivityStatement />
+        </div>
+        <div className='fixed bg-white bottom-0 left-0 right-0 p-[15px]'>
+          <FunctionButton
+            text={"搶先登記"}
+            textColor={"white"}
+            textPosition={"center"}
+            bgColor={"#ff5353"}
+            letterSpacing={2}
+            width={"full"}
+            clickFn={() => navigate("/reserve/submitForm")}
+          />
+        </div>
       </div>
-      <div className='fixed bg-white bottom-0 left-0 right-0 p-[15px]'>
-        <FunctionButton
-          text={"搶先登記"}
-          textColor={"white"}
-          textPosition={"center"}
-          bgColor={"#ff5353"}
-          letterSpacing={2}
-          width={"full"}
-          clickFn={() => navigate("/reserve/submitForm")}
-        />
-      </div>
-    </div>
+    </>
   );
 }
 
