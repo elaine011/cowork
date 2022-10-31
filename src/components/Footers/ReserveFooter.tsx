58a3abb67904439ba47bf6f2e840ce7ce1d10ccc
Xiang <wiwi19991214@gmail.com>
2022-10-31 15:14:46 +0800

    fix hint text in footer

--- src/components/Footers/ReserveFooter.tsx
+++ src/components/Footers/ReserveFooter.tsx
@@ -22,13 +22,13 @@ function ReserveFooter({
       className={`text-center fixed border-t-[2px] border-solid border-borderGray bg-white bottom-0 left-0 right-0 ${
         hint ? "py-[5px]" : "py-[14px]"
       } px-[14px]`}>
-      <div className='max-w-[1080px] mx-auto'>
-        <div className='flex' style={{ justifyContent: wrapperContext }}>
+      <div className="max-w-[1080px] mx-auto">
+        <div className="flex" style={{ justifyContent: wrapperContext }}>
           {hint && (
-            <p className='text-[20px] mb-[5px] text-primaryRed'>{hint}</p>
+            <p className="text-[20px] mb-[5px] text-primaryRed">{hint}</p>
           )}
-          {price && (
-            <p className='text-[20px] mb-[5px] text-primaryRed'>NT.{price}</p>
+          {price !== undefined && (
+            <p className="text-[20px] mb-[5px] text-primaryRed">NT.{price}</p>
           )}
         </div>
 
