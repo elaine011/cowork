4a26ce037ab7a732b1e65afd70eb72d2e3502a82
Xiang <wiwi19991214@gmail.com>
2022-10-31 09:18:30 +0800

    adjust page backgroundColor

--- src/pages/Reserve/SubmitForm/SubmitForm.tsx
+++ src/pages/Reserve/SubmitForm/SubmitForm.tsx
@@ -4,6 +4,7 @@ import ReserveFooter from "../../../components/Footers/ReserveFooter";
 function SubmitForm() {
   return (
     <>
+      <div className='fixed right-0 left-0 top-0 bottom-0 bg-secondaryPageBackgroundGray z-[-50]'></div>
       <main className='md:px-4 md:bg-primaryPageBackgroundGray md:h-[100vh]'>
         <Header />
         <div className='pt-[17px] bg-white md:py-[60px] max-w-[1080px] mx-auto'>
