95bf6f5c88b82df2870fbfc276410f958dead2b8
Xiang <wiwi19991214@gmail.com>
2022-11-14 11:19:44 +0800

    fix resereve header bug

--- src/components/Headers/ReserveHeader.tsx
+++ src/components/Headers/ReserveHeader.tsx
@@ -5,13 +5,18 @@ function ReserveHeader() {
   const navigate = useNavigate();
   return (
     <header className="bg-primaryRed pl-[13px] h-[57px] md:h-[82px]">
-      <div className="max-w-[1106px] mx-auto flex items-center h-full">
-        <img
-          src={Logo}
-          alt="Logo圖"
-          className="cursor-pointer"
-          onClick={() => navigate("/")}
-        />
+      <div
+        className="max-w-[1106px] mx-auto flex items-center h-full"
+        onClick={() => {
+          const data = JSON.parse(
+            sessionStorage.getItem("registerInfo") as string
+          );
+          if ("product" in data && "user" in data) {
+            sessionStorage.clear();
+          }
+          navigate("/");
+        }}>
+        <img src={Logo} alt="Logo圖" className="cursor-pointer" />
       </div>
     </header>
   );
